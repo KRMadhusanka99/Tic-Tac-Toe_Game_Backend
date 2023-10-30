719a1fd11ada86f29c2d9dde955396e5ce2eb8a2
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-10-31 01:07:24 +0530

    add repository directory and create playerRepo interface

---
+++ src/main/java/com/example/tictactoe/repository/PlayerRepository.java
@@ -0,0 +1,7 @@
+package com.example.tictactoe.repository;
+
+import com.example.tictactoe.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player,Integer> {
+}
