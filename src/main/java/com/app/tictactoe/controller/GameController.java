3c6e75fe85293fe729d9fc5e232d36d769a98003
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-11-15 00:26:53 +0530

    add reset API for game reset

--- src/main/java/com/app/tictactoe/controller/GameController.java
+++ src/main/java/com/app/tictactoe/controller/GameController.java
@@ -9,10 +9,7 @@ import com.app.tictactoe.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/game")
@@ -29,5 +26,10 @@ public class GameController {
         }catch (PlayerNotExistException | PlayerActiveException e){
             return ResponseEntity.status(HttpStatus.CONFLICT).body(new GameResponseDTO(e.getMessage()));
         }
+    }
+
+    @PostMapping("/reset/{playerName}")
+    public ResponseEntity<GameResponseDTO> resetGame(@PathVariable String playerName){
+        
     }
 }
